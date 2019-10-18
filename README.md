fcb4365ad766e66518028e5d329edaa27510494c
Alexey Strakh <alexey.strakh@microsoft.com>
2019-10-18 15:48:01 -0700

    implement kotlin xamarin binding for bubbles picker archive

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# xamarin-binding-kotlin-framework
+# Xamarin.Android Binding for a Kotlin Android Archive
+
+This reposiory contains a kotlin bubles picker library built and bound using a xamarin.android project and then referenced and used by a xamarin application
+
+## Kotlin and Bubles Picker
+
+We use the bubbles picker as an example of a kotlin library, copied source and updated dependencies in order to build latest version of the android archive (.aar). The archived has been place into the `Kotlin/VendorsFramework/` folder
+
+## Xamarin Binding
+
+The bubbles picker android archive copied to the Jar folder of the project and a few transformations applied to exclude not used dependecies from being generated as C# classes. Two issues with the `getBackground` and `setBackground` has been fixed to reflect real kotlin intent. Two important packages have been added to support kotlin libraries:
+
+- [Android Kotlin BindingSupport Nuget](https://www.nuget.org/packages/Xamarin.Kotlin.BindingSupport/)
+- [Android Kotling StdLib Nuget](https://www.nuget.org/packages/Xamarin.Kotlin.StdLib/)
+
+## Xamarin App
+
+A simple xamarin app has been created in order to instanciate and render the bubbles picker control. The project references the bubbles picker xamarin.android binding and instantiate the control in the same way as the native sample application does.
+
+## Demo
+
+<img src="SolutionItems/bubbles-picker.gif" alt="bubbles picker demo" height="350" style="display:inline-block;" />
---
+++ Xamarin/testBubblePicker/MainActivity.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Com.Igalata.Bubblepicker;
+using Com.Igalata.Bubblepicker.Adapter;
+using Com.Igalata.Bubblepicker.Model;
+using Com.Igalata.Bubblepicker.Rendering;
+
+namespace testBubblePicker
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            var picker = FindViewById<BubblePicker>(Resource.Id.picker);
+            picker.BubbleSize = 20;
+            picker.Adapter = new BubblePickerAdapter();
+            picker.Listener = new BubblePickerListener(picker);
+
+
+            //        return PickerItem().apply {
+            //            title = titles[position]
+            //            gradient = BubbleGradient(colors.getColor((position * 2) % 8, 0),
+            //                    colors.getColor((position * 2) % 8 + 1, 0), BubbleGradient.VERTICAL)
+            //            typeface = mediumTypeface
+            //            textColor = ContextCompat.getColor(this@DemoActivity, android.R.color.white)
+            //            backgroundImage = ContextCompat.getDrawable(this@DemoActivity, images.getResourceId(position, 0))
+            //                    }
+            //        }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            var result = "Please create your native control";
+            //var control = new Com.Microsoft.CoolcontrolsLibrary.CoolKotlinControl();
+            //var result = control.DoIt("Alexey");
+
+            var control = new BubblePicker(this);
+            result = control.ToString();
+
+            View view = (View)sender;
+            Snackbar.Make(view, result, Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public class BubblePickerAdapter : Java.Lang.Object, IBubblePickerAdapter
+    {
+        private List<string> _bubbles = new List<string>();
+
+        public int TotalCount => _bubbles.Count;
+
+        public BubblePickerAdapter()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _bubbles.Add($"Item {i}");
+            }
+        }
+
+        public PickerItem GetItem(int itemIndex)
+        {
+            if (itemIndex < 0 || itemIndex >= _bubbles.Count)
+                return null;
+
+            var result = _bubbles[itemIndex];
+            var item = new PickerItem(result);
+            return item;
+        }
+    }
+
+    public class BubblePickerListener : Java.Lang.Object, IBubblePickerListener
+    {
+        public View Picker { get; }
+
+        public BubblePickerListener(View picker)
+        {
+            Picker = picker;
+        }
+
+        public void OnBubbleDeselected(PickerItem item)
+        {
+            System.Diagnostics.Debug.WriteLine($"Deselected: {item.Title}");
+            Snackbar.Make(Picker, $"Deselected: {item.Title}", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null)
+                .Show();
+        }
+
+        public void OnBubbleSelected(PickerItem item)
+        {
+            System.Diagnostics.Debug.WriteLine($"Selected: {item.Title}");
+            Snackbar.Make(Picker, $"Selected: {item.Title}", Snackbar.LengthLong)
+               .SetAction("Action", (Android.Views.View.IOnClickListener)null)
+               .Show();
+        }
+    }
+}
+
